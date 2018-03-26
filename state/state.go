18601abc27e7ec8981bd5218d4b6db6afe44463f
Dafydd Rosser <drosser@companieshouse.gov.uk>
2018-03-26 16:39:48 +0100

    Added ability to get the session from the Cache
    Resolves FAPI-2242

--- state/state.go
+++ state/state.go
@@ -6,6 +6,7 @@ from the cache.
 package state
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/companieshouse/chs.go/log"
@@ -43,19 +44,57 @@ func (s *Store) Load(req *http.Request) {
 	cookie, _ := s.getCookieFromRequest(req)
 
 	s.validateCookieSignature(req, cookie.Value)
+	s.extractAndValidateCookieSignatureParts(req, cookie.Value)
 
+	storedSession, err := s.getStoredSession(req)
+
+	if err != nil {
+		log.InfoR(req, err.Error())
+	}
+
+	if storedSession == nil {
+
+	}
 }
 
 func (s *Store) Store() {
 
 }
 
+//Delete will clear the requested session from the backing store. Note: Delete
+//does not clear the loaded session. The Clear method will take care of that.
+//If the string passed in is nil, it will delete the session with an id the same
+//as that of s.ID
+func (s *Store) Delete(req *http.Request, id *string) {
+	sessionID := s.ID
+
+	if id != nil && len(*id) > 0 {
+		sessionID = *id
+	}
+
+	cache := s.getRedisClientFromCache()
+
+	_, err := cache.connection.Del(sessionID).Result()
+
+	if err != nil {
+		log.InfoR(req, err.Error())
+	}
+}
+
+//Clear destroys the current loaded session and removes it from the backing
+//store. It will also regenerate the session ID.
+func (s *Store) Clear(req *http.Request) {
+	s.Data = nil
+	s.Delete(req, nil) //Delete the previously stored Session
+	s.regenerateID()
+}
+
 func (s *Store) regenerateID() {
 
 }
 
-func (s *Store) generateSignature() {
-
+func (s *Store) generateSignature() string {
+	return ""
 }
 
 //getCookieFromRequest will attempt to pull the Cookie from the request. If err
@@ -79,7 +118,46 @@ func (s *Store) validateCookieSignature(req *http.Request, cookieSignature strin
 
 	if len(cookieSignature) != cookieValueLength {
 		log.InfoR(req, "Cookie signature is not the correct length")
+
+		s.Clear(req)
+		return
 	}
+}
+
+//extractAndValidateCookieSignatureParts will split the cookieSignature into
+//two parts, and set the first part to s.ID, with the second part being validated
+//against a generated ID.
+func (s *Store) extractAndValidateCookieSignatureParts(req *http.Request, cookieSignature string) {
+	s.ID = cookieSignature[0:signatureStart]
+	sig := cookieSignature[signatureStart:len(cookieSignature)]
+
+	//Validate signature is the same
+	if sig != s.generateSignature() {
+		s.Clear(req)
+		return
+	}
+}
+
+//getRedisClientFromCache will construct a new Cache and invoke getRedisClient.
+func (s *Store) getRedisClientFromCache() *Cache {
+	cache := &Cache{}
+	cache.getRedisClient()
+
+	return cache
+}
+
+//getStoredSession will get the session from the Cache, and validate it.
+//If it is invalid, it will return an error.
+func (s *Store) getStoredSession(req *http.Request) ([]byte, error) {
+	cache := s.getRedisClientFromCache()
+	storedSession := cache.getSession(s.ID)
+
+	if len(storedSession) == 0 {
+		err := errors.New("There is no stored session")
+		return nil, err
+	}
+
+	return storedSession, nil
 }
 
 /*
@@ -90,8 +168,9 @@ func (c *Cache) getRedisClient() {
 
 }
 
-func (c *Cache) getSession() {
-
+func (c *Cache) getSession(id string) []byte {
+	var a []byte
+	return a
 }
 
 func (c *Cache) setSession() {
